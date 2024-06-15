150bc38cb14af193ded9ffe2d9e4bc191c39d9e4
Scott Fennell <slfennell01@gmail.com>
2024-06-14 20:08:19 -0400

    Implement interactive behaviors with d3.js
    Re-structure code in html and style. Added new js code

--- script.js
+++ script.js
@@ -2,4 +2,17 @@
 
 function main(){
     // d3 code goes here
+
+    const container = d3.select('#container'); // container in html body to store data point divs
+    const myData = []; // empty space
+    for (let i = 0; i<5; i++){
+        myData.push(i)
+    }
+    
+    container.select('body')
+        .select('div')
+        .data(myData)
+        .enter()
+        .append('rect')
+
 }
